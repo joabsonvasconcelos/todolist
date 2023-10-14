23f6e313f8f33ebb4eec2e4caa4e63ad05fdd92d
Joabson Vasconcelos <119760277+joabsonvasconcelos@users.noreply.github.com>
2023-10-14 19:39:29 -0300

    update

--- src/main/java/br/com/tecnojobs/todolist/user/UserController.java
+++ src/main/java/br/com/tecnojobs/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.tecnojobs.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,21 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /**
-     * String (texto)
-     * Integar (int) numeros inteiros
-     * Double (double) números 0,0000
-     * Float (float) Números  0.000
-     * char ( A C )
-     * Date (Data)
-     * void
-     * 
-     * body
-     */
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    @Autowired
+    private IUserRepository userRepository;
 
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null ){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
+
      
 }
